28b271bbe5bfdac03f12a2216450f790b5e237a3
bibamet <spr1tee1337@gmail.com>
2024-05-31 21:56:57 +0300

    fixed docker compose

--- backend/auth-service/src/main/java/ru/itpark/authservice/presentation/web/TestController.java
+++ backend/auth-service/src/main/java/ru/itpark/authservice/presentation/web/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @PreAuthorize("isAuthenticated()")
+//    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public String test() {
         return "вы авторизованы";
