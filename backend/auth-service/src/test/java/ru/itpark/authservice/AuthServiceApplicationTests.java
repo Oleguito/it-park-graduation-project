63a5c2742b1ec26a6d1e3bfcae65589a6b863cf9
bibamet <spr1tee1337@gmail.com>
2024-06-09 15:45:53 +0300

    added JOOQ support

--- backend/auth-service/src/test/java/ru/itpark/authservice/AuthServiceApplicationTests.java
+++ backend/auth-service/src/test/java/ru/itpark/authservice/AuthServiceApplicationTests.java
@@ -1,13 +1,39 @@
 package ru.itpark.authservice;
 
+import lombok.Value;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
 
-// @SpringBootTest
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SpringBootTest
 class AuthServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void someTest() {
+
+        String userName = "authservice";
+        String password = "12345";
+        String url = "jdbc:postgresql://collaborative-project-postgres-db:5432/authservicedb";
+
+        System.out.println("Перед подключением JOOQ");
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            System.out.println("подключились connected");
+        }
+
+        // For the sake of this tutorial, let's keep exception handling simple
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
