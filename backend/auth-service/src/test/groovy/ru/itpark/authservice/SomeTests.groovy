92df73fa488d63862f4df3553bd1e6662f31d743
Oleguito <tenzoriator@rambler.ru>
2024-06-11 16:58:25 +0300

    добавил удобства в тесты на русском языке

--- backend/auth-service/src/test/groovy/ru/itpark/authservice/SomeTests.groovy
+++ backend/auth-service/src/test/groovy/ru/itpark/authservice/SomeTests.groovy
@@ -2,12 +2,17 @@ package ru.itpark.authservice
 
 import ru.itpark.authservice.domain.user.dto.queries.UserQuery
 import ru.itpark.authservice.infrastructure.config.security.keycloak.KeycloakClient
+import spock.lang.Narrative
 import spock.lang.Specification
 
+@Narrative("""
+    Большой длинный и удобный текст 
+    который подробно объясняет че за херня тут творится
+""")
 class SomeTests extends Specification {
 
     def keycloakClient = new KeycloakClient()
-    UserQuery userQuery
+    def userQuery
 
     def setup() {
         userQuery = new UserQuery()
@@ -15,7 +20,7 @@ class SomeTests extends Specification {
         userQuery.setPassword("12345")
     }
 
-    def "tesskjhfds"() {
+    def "Что-то да проверяем.. не?"() {
         expect:
         1 == 1
     }
@@ -24,23 +29,24 @@ class SomeTests extends Specification {
         setup: "KeycloakClient class"
 
         when:
-        def response = keycloakClient.getUserInfo(userQuery)
+        def response = keycloakClient.getUserInfo(userQuery as UserQuery)
         println response['access_token']
 
         then:
         response['access_token'] != null
     }
 
-    def "UsersController handle to revoke Jwt token via keycloak"() {
+
+    def "UsersController handle to revoke Jwt token via keycloak status 200"() {
         given:
-        def adminToken = keycloakClient.createUserToken(userQuery)
+        def adminToken = keycloakClient.createUserToken(userQuery as UserQuery)
         def toRevokeToken = adminToken
 
         when:
         def response = keycloakClient.revokeUserToken(adminToken, toRevokeToken)
         println response
 
-        then:
+        then: "response should be status 200 for success"
         response
     }
 }
