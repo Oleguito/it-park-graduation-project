06df2f25a2c14f72ab8ce2d2d040d5e65fdd7346
bibamet <spr1tee1337@gmail.com>
2024-06-23 01:21:53 +0300

    MODIFY THE orun.bat BY THE INSERTING AUTO REPEAT INSTRUCTIONS WHILE APP IS NOT AVAILABLE

--- infrastructure/orun.bat
+++ infrastructure/orun.bat
@@ -5,5 +5,13 @@ docker compose stop
 docker compose up -d
 
 cd ../backend/auth-service
-mvnw clean package spring-boot:run -Dspring-boot.run.profiles=dev
+
+:retry
+call mvnw clean install spring-boot:run -Dspring-boot.run.profiles=dev
+
+    if %ERRORLEVEL% neq 0 (
+          echo Error occurred during mvnw clean install, but continuing...
+          goto retry
+      )
+
 rem spring-boot:run
