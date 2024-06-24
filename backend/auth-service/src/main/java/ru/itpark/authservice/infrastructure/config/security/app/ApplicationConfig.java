5e8bc2b0951853ccef2094679d366dfb268086c1
Oleguito <tenzoriator@rambler.ru>
2024-06-24 16:27:06 +0300

    Написал пару тестов для сервиса авторизации применил тестконтейнер

--- backend/auth-service/src/main/java/ru/itpark/authservice/infrastructure/config/security/app/ApplicationConfig.java
+++ backend/auth-service/src/main/java/ru/itpark/authservice/infrastructure/config/security/app/ApplicationConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,5 +35,6 @@ public class ApplicationConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
+
 }
 
---
+++ backend/auth-service/src/test/groovy/oleg/LearnSpockTests.groovy
@@ -0,0 +1,43 @@
+package oleg
+
+import spock.lang.Specification
+
+
+class LearnSpockTests extends Specification {
+
+    Publisher publisher = new Publisher()
+    Subscriber subscriber = Mock()
+    Subscriber subscriber2 = Mock()
+
+    def "Еще кое-чето из спока"() {
+        when:
+        publisher.send("hello")
+
+        then: "Без блока interaction будет ошибка"
+        interaction {
+            def message = "hello"
+            /* _ означает "что-то одно, но неважно что"
+            * то есть любое число вызовов */
+            _ * subscriber.receive(message)
+        }
+    }
+
+    def "Что-то да проверяем.. не?"() {
+        def a = 2
+        def b = a * 3
+        expect:
+        a + b == 8
+    }
+}
+
+class Publisher {
+    def send(String message) {
+
+    }
+}
+
+class Subscriber {
+    def receive(String message) {
+
+    }
+}
