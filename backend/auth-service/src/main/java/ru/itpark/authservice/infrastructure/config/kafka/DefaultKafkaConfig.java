ab0de7123c429a761510bd92be5487defd90ca44
bibamet <spr1tee1337@gmail.com>
2024-07-06 11:35:48 +0300

    added KAFKA

--- backend/auth-service/src/main/java/ru/itpark/authservice/infrastructure/config/kafka/DefaultKafkaConfig.java
+++ backend/auth-service/src/main/java/ru/itpark/authservice/infrastructure/config/kafka/DefaultKafkaConfig.java
@@ -1,2 +1,33 @@
-package ru.itpark.authservice.infrastructure.config.kafka;public class DefaultKafkaConfig {
+package ru.itpark.authservice.infrastructure.config.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class DefaultKafkaConfig {
+
+//    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("localhost:9092")
+    private String bootstrap;
+//    @Value("${spring.kafka.consumer.group-id}")
+    @Value("my-group")
+    private String groupId;
+
+    protected Map<String, Object> getProducerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
 }
+
