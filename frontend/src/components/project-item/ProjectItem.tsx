b342ec7b902350c9e5551f3d1461c5582c5c42d3
Oleguito <tenzoriator@rambler.ru>
2024-07-22 16:55:21 +0300

    Нарисовал "мои проекты" и "добавить проект"; добавил кнопку "приглашения"; форма добавления проверяется zod

---
+++ frontend/src/components/project-item/ProjectItem.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import { Button } from '../ui/button';
+
+import css from './styles.module.css'
+
+export type Props = {
+
+    projectId: number;
+    projectDescription: string;
+}
+
+const addMemberHandler = () => {};
+
+const deleteMemberHandler = () => {};
+
+const ProjectItem = ({ props }: { props: Props }) => {
+  return (
+      <>
+          <div>
+              <div>ProjectItem</div>
+              <div className={css['project-item-upper-part']}>
+                  <div>Project #{props.projectId}</div>
+                  <textarea readOnly={true} value={props.projectDescription} className={css['description-box']} />
+              </div>
+          </div>
+      </>
+  );
+}
+
+export default ProjectItem
