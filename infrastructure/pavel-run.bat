0665f53218f0dac2aff974a9fbb5430bf38bb9d0
bibamet <spr1tee1337@gmail.com>
2024-08-15 23:00:00 +0300

    пофиксил запуск, попробуй запустить через мой батник - pavel-run.bat

--- infrastructure/pavel-run.bat
+++ infrastructure/pavel-run.bat
@@ -1,10 +1,10 @@
-@echo off
-chcp 65001 >nul
-cls
-docker compose stop
-docker compose up -d
+rem @echo off
+rem chcp 65001 >nul
+rem cls
+rem docker compose stop
+rem docker compose up -d
 
-cd ../backend/auth-service
+rem cd ../backend/auth-service
 
 rem :retry
 rem call mvnw clean install spring-boot:run -Dspring-boot.run.profiles=dev
@@ -15,3 +15,21 @@ rem          goto retry
 rem      )
 
 rem spring-boot:run
+
+@echo off
+chcp 65001 >nul
+echo Остановка сервисов...
+docker compose stop
+echo Подготовка к запуску...
+docker compose up -d collaborative-project-postgres-db liquibase liquibase-auth liquibase-notification
+echo Ожидание заверешения подготовки
+docker compose wait liquibase liquibase-auth liquibase-notification
+
+REM Проверяем, успешно ли завершилась предыдущая команда
+if %errorlevel% equ 0 (
+    echo Запуск основных сервисов...
+    docker compose up
+) else (
+    echo Liquibase services failed. Exiting.
+    exit /b 1
+)
