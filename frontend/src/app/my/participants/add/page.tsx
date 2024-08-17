def16c0b633dba3ae78993b5113986edde235970
Oleguito <tenzoriator@rambler.ru>
2024-08-17 12:33:27 +0300

    zod проверяет введенный имейл

--- frontend/src/app/my/participants/add/page.tsx
+++ frontend/src/app/my/participants/add/page.tsx
@@ -1,12 +1,33 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { useRef } from 'react';
+import { z } from "zod";
 import css from './styles.module.css';
+
+const schema = z.object({
+    email_input_box: z
+		.string()
+		.email()
+});
 
 const AddParticipantPage = () => {
 
-	const handleAddParticipant = () => {
-		
+	const emailRef = useRef(null);
+
+	const handleAddParticipant = (event) => {
+		event.preventDefault();
+		let parsed;
+		try {
+			parsed = schema.parse({
+            email_input_box: emailRef.current.value,
+        });	
+		} catch (error) {
+			console.log(error);
+			emailRef.current.value = '';
+		}
+		console.log(emailRef.current.value);
+		console.log(parsed);
 	}
 
     return (
@@ -18,6 +39,7 @@ const AddParticipantPage = () => {
                 <div className="flex">
                     <label htmlFor="project-title">Email:</label>
                     <input
+                        ref={emailRef}
                         type="text"
                         id="email_input_box"
                         name="email_input_box"
@@ -25,8 +47,11 @@ const AddParticipantPage = () => {
                         className={css.email_input_box}
                     ></input>
                 </div>
-                <div className='flex justify-center'>
-                    <Button onClick={handleAddParticipant} className="m-1 bg-slate-500 text-white font-bold rounded-sm">
+                <div className="flex justify-center">
+                    <Button
+                        onClick={handleAddParticipant}
+                        className="m-1 bg-slate-500 text-white font-bold rounded-sm"
+                    >
                         Добавить участника
                     </Button>
                 </div>
