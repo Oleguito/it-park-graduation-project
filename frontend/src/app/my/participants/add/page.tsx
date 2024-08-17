73422ae4ebbaa4caf0b460744ce99ff2e331b7eb
Oleguito <tenzoriator@rambler.ru>
2024-08-17 11:58:42 +0300

    добавил формочку добавления участника

--- frontend/src/app/my/participants/add/page.tsx
+++ frontend/src/app/my/participants/add/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+'use client';
+
+import { Button } from '@/components/ui/button';
+import css from './styles.module.css';
 
 const AddParticipantPage = () => {
-  return (
-    <div>AddParticipantPage</div>
-  )
-}
 
-export default AddParticipantPage
+	const handleAddParticipant = () => {
+		
+	}
+
+    return (
+        <>
+            <div>AddParticipantPage</div>
+
+            <div className={`w-full`}>
+                <p>Введите адрес электронной почты участника</p>
+                <div className="flex">
+                    <label htmlFor="project-title">Email:</label>
+                    <input
+                        type="text"
+                        id="email_input_box"
+                        name="email_input_box"
+                        placeholder="Адрес электронной почты"
+                        className={css.email_input_box}
+                    ></input>
+                </div>
+                <div className='flex justify-center'>
+                    <Button onClick={handleAddParticipant} className="m-1 bg-slate-500 text-white font-bold rounded-sm">
+                        Добавить участника
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AddParticipantPage;
