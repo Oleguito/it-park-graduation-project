c153b95eaca350b4e9a1d63dcade63500dc35cc8
Oleguito <tenzoriator@rambler.ru>
2024-08-27 11:17:14 +0300

    создал document-service

---
+++ backend/document-service/src/test/java/ru/itpark/documentservice/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itpark.documentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
